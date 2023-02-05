7031fb00b5ed656c548051baec68235decd3633b
Hardik-creator108 <prajapatihardik134@gmail.com>
2023-02-05 12:51:34 -0500

    Updates

---
+++ precache-manifest.1daf0ab652effcf70604e1a3cb4230c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6984e2949a4e43f5598f",
+    "url": "/static/css/main.ed94ace8.chunk.css"
+  },
+  {
+    "revision": "6984e2949a4e43f5598f",
+    "url": "/static/js/main.6984e294.chunk.js"
+  },
+  {
+    "revision": "8661c4af1aa50a4cec73",
+    "url": "/static/js/1.8661c4af.chunk.js"
+  },
+  {
+    "revision": "4ed8035429fa5f837896",
+    "url": "/static/js/2.4ed80354.chunk.js"
+  },
+  {
+    "revision": "c5827c058dc15ddfd63a",
+    "url": "/static/js/runtime~main.c5827c05.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "71a1f899cc4b5b5f93313bc43b8a94b6",
+    "url": "/static/media/logo.71a1f899.jpg"
+  },
+  {
+    "revision": "1881fcdd26a1036ec179e139e04ccb22",
+    "url": "/index.html"
+  }
+];
